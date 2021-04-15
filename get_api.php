81bc6157a706b5f40925fadcd7943ab3213798e9
KeyanBawazir <key42pc@gmail.com>
2021-04-16 02:11:07 +0300

    to get news api  and to convert to php object

---
+++ get_api.php
@@ -0,0 +1,8 @@
+<?php
+// ===== This google news api for exaple =====
+$url = "https://newsapi.org/v2/everything?q=bitcoin&from=2021-03-15&sortBy=publishedAt&apiKey=0a24a4e7148c4fb2b44c60ac4ca188b6";
+// ===== To get as Json file =====
+$response = file_get_contents($url);
+// ===== Decode Json file to object in php =====
+$data = json_decode($response);
+
